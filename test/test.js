87a911289a68cb96c4d36ff6920fb938a29a00da
ctudel <christudela@u.boisestate.edu>
2024-08-30 01:57:26 -0600

    Added about me page

--- test/test.js
+++ test/test.js
@@ -1,16 +1,10 @@
 import {sayHello} from '../js/main.js';
 
-
-
-
 QUnit.module('hello', function() {
 
     QUnit.test('make sure the hello function says hello', function(assert) {
         var result = sayHello();
         assert.equal(result, 'hello');
     });
-
-
-
 
 });
